410fd24f268cf24f783c99406dbab8bc86192e46
Danil <danilbuslenko@gmail.com>
2023-12-03 11:51:52 +0200

    Updates

--- build.js
+++ build.js
@@ -1532,7 +1532,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	
 /******/ 	/* webpack/runtime/publicPath */
 /******/ 	(() => {
-/******/ 		__webpack_require__.p = "/";
+/******/ 		__webpack_require__.p = "/dist/";
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/jsonp chunk loading */

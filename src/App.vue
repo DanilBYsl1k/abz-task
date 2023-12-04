4302d1d364f252f3da9e53f1008d47ffffe7667f
Danil <danilbuslenko@gmail.com>
2023-12-04 21:38:53 +0200

    finish

--- src/App.vue
+++ src/App.vue
@@ -26,12 +26,22 @@ import MainComponent from "./views/Main.vue";
 
 body {
   background: $background_color;
+  position: relative;
 }
 
 a {
   text-decoration: none;
   color: black;
   text-align: center;
+}
+
+.bg-img {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  z-index: -1;
 }
 
 .container {
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,6 +1,7 @@
 <template>
     <section class="test-assignment">
       <div class="test-assignment__container container fx-column justify-content-center align-items-center">
+        <img src="imgs/background.jpg" class="bg-img"  alt="">
         <div class="test-assignment__box">
           <h1>Test assignment for front-end developer</h1>
           <p class="test-assignment__info">What defines a good front-end developer is one that has skilled knowledge of HTML, CSS, JS with a vast understanding of User design thinking as they'll be building web interfaces with accessibility in mind. They should also be excited to learn, as the world of Front-End Development keeps evolving.</p>
@@ -137,7 +138,8 @@ onMounted(async () => {
 .test-assignment {
   &__container {
     text-align: center;
-    background: url('../assets/imgs/background.jpg') no-repeat center;
+    position: relative;
+    // background: url('../../public/imgs/background.jpg') no-repeat center;
     height: 650px;
   }
   
